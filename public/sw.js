0b1943256902e820d9662e685c2948ead3368a32
mark-williams <markwill@hotmail.co.uk>
2017-05-28 14:51:11 +0100

    Now responds with 200 when offline

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,15 @@ self.addEventListener('install', function() {
   console.log('Service worker installed at ' + new Date().toLocaleTimeString());
 });
 
-
 self.addEventListener('activate', function() {
   console.log('Service worker activated at ' + new Date().toLocaleTimeString());
+});
+
+self.addEventListener('fetch', function(event) {
+  if (!navigator.onLine) {
+    event.respondWith(new Response('<h2>Currently offline</h2>', { headers: { 'Content-Type': 'text/html' } }));
+    return;
+  }
+
+  console.log('fetching: ' + event.request.url);
 });

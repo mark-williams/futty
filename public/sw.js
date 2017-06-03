582cb71b35248f8a7e17be2e8deb9af4c182f5eb
mark-williams <markwill@hotmail.co.uk>
2017-06-03 17:02:05 +0100

    Cache offline content when service worker installs

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,47 @@
 
-self.addEventListener('install', function() {
+const cacheVersion = 'v1';
+const cacheItems = [
+  'favicon-16x16.ico',
+  'favicon-32x32.ico',
+  'favicon.ico',
+  'app.css',
+  'index.css',
+  'offline.html'
+];
+
+self.addEventListener('install', function () {
   console.log('Service worker installed at ' + new Date().toLocaleTimeString());
 });
 
-self.addEventListener('activate', function() {
+self.addEventListener('activate', function () {
   console.log('Service worker activated at ' + new Date().toLocaleTimeString());
 });
 
-// self.addEventListener('fetch', function(event) {
-//   if (!navigator.onLine) {
-//     event.respondWith(new Response('<h2>Currently offline</h2>', { headers: { 'Content-Type': 'text/html' } }));
-//     return;
-//   }
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(cacheVersion)
+      .then(function (cache) {
+        console.log('Initialising caches...');
+        return cache.addAll(cacheItems);
+      })
+  );
+});
 
-//   console.log('fetching: ' + event.request.url);
-// });
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (res) {
+        if (res) {
+          return res;
+        }
+
+        if (!navigator.onLine) {
+          return caches.match(new Request('offline.html'));
+          //return new Response('<h2>Currently offline</h2>', { headers: { 'Content-Type': 'text/html' } });
+        }
+
+        console.log('fetching: ' + event.request.url);
+        return fetch(event.request);
+      })
+  );
+});

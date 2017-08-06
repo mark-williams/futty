74d635b2bf4829a1d01454f3f95780b280f3af07
mark-williams <markwill@hotmail.co.uk>
2017-08-06 10:54:52 +0100

    Removed emotion - doesn't play too well with unejected create-react-app.

--- src/components/Fixture.js
+++ src/components/Fixture.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+//import styled from 'emotion/react';
 import getShortName from '../utils/team-utils';
+
+
+// const StyledFixture = styled.div`
+//   color: red;
+// `;
+
+// const Fixture = ({fixture}) => {
+//   return <StyledFixture>blah</StyledFixture>;
+// }
 
 const Fixture = ({fixture}) => {
   const date = new Date(fixture.date).toLocaleDateString();
   return (
-    <div className="fixture" key={date}>
+    <div key={date}>
       <div className="fixture-date">
         {date}
       </div>

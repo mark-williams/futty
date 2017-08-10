4ea27bb1db88022b265dc3c6415494b95c70c00a
mark-williams <markwill@hotmail.co.uk>
2017-08-10 09:38:30 +0100

    Added navigation (well, a home link!).

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import routes from './routes';
+import { colours } from './style';
 import './style/App.css';
 
 const Container = styled.div`
@@ -17,6 +18,14 @@ const Header = styled.div`
   color: hsl(0, 0%, 91%);
 `;
 
+const Navigation = styled.div`
+  background-color: ${colours.secondary};
+  height: 20px;
+  padding: 4px;
+  color: #fff;
+  text-align: left;
+`;
+
 class App extends Component {
   render() {
     return (
@@ -25,15 +34,29 @@ class App extends Component {
           <h2>Welcome to Futty</h2>
           <p>Get your football stats here!</p>
         </Header>
-        <div>
-          <Router>
-            {routes}
-          </Router>
-        </div>
+        <Router>
+          <Route path="/" component={MainContainer}>
+           
+          </Route>
+         </Router>
+        
       </Container>
     );
   }
 }
+
+const MainContainer = (props) => (
+   <div>
+     <Nav />
+     {routes}
+  </div>
+);
+
+const Nav = () => (
+  <Navigation>
+    <NavLink to={'/'}>Home</NavLink>
+  </Navigation>
+);
 
 export default App;
 
